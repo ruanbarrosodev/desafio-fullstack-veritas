0bedc9e742150976a10cd75bcb9a01cf4d75d2a7
Ruan Barroso <ruan.x14@gmail.com>
2026-08-12 20:16:28 -0400

    starting initial project structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div id="teste" >
+        oi teste
+      </div>
+    </>
+  )
+}
+
+export default App
